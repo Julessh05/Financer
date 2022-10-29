8c7beaff425a038a9fa3b1ea11cbaa7d5185d4c8
Julian Schumacher <Jules.media@outlook.de>
2022-10-29 19:00:59 +0200

    Add Files

---
+++ ios/Financer/Financer/FinancerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinancerApp.swift
+//  Financer
+//
+//  Created by Julian Schumacher on 29.10.22.
+//
+
+import SwiftUI
+
+@main
+struct FinancerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
