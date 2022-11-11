0ec15dee035974ff51c776572fe6c1d72f3f9f11
Julian Schumacher <Jules.media@outlook.de>
2022-11-11 07:57:19 +0100

    Update Project

--- ios/Financer/Financer/Logic/EnviromentalExtensions.swift
+++ ios/Financer/Financer/Logic/EnviromentalExtensions.swift
@@ -18,6 +18,9 @@ private struct UserKey : EnvironmentKey {
 /// Extension on the EnvironmentValues to provide
 /// the current User to the Environment.
 internal extension EnvironmentValues {
+    /// The current User of this App.
+    /// The defaultValue of this Environmental
+    /// Value is an empty User.
     var currentUser : User {
         get { self[UserKey.self] }
         set { self[UserKey.self] = newValue }
