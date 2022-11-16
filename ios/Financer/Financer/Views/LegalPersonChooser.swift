52f904fb6d503f3db94619d64ea37f458090669d
Julian Schumacher <Jules.media@outlook.de>
2022-11-16 07:38:34 +0100

    Add LegalPersonChooser

---
+++ ios/Financer/Financer/Views/LegalPersonChooser.swift
@@ -0,0 +1,31 @@
+//
+//  LegalPersonChooser.swift
+//  Financer
+//
+//  Created by Julian Schumacher on 16.11.22.
+//
+
+import SwiftUI
+
+/// View to choose which legal Person should be created
+struct LegalPersonChooser: View {
+    var body: some View {
+        HStack {
+            VStack {
+                Image(sys)
+            }
+            VStack {
+
+            }
+            VStack {
+
+            }
+        }
+    }
+}
+
+struct LegalPersonChooser_Previews: PreviewProvider {
+    static var previews: some View {
+        LegalPersonChooser()
+    }
+}
