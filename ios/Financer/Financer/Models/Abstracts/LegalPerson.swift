fdcdfde42a09c60fe802b1a426d1edd564663f70
Julian Schumacher <Jules.media@outlook.de>
2022-11-30 18:26:54 +0100

    Update Code

--- ios/Financer/Financer/Models/Abstracts/LegalPerson.swift
+++ ios/Financer/Financer/Models/Abstracts/LegalPerson.swift
@@ -28,57 +28,53 @@ internal class LegalPerson : Equatable, Identifiable, Codable {
         case person
     }
 
-    /// The Relation if this Legal Person
-    /// is a Company
-    internal enum CompanyRelation : String, Relation {
-        var id : Self { self }
+    internal enum Relation : String {
+        /// The Relation if this Legal Person
+        /// is a Company
+        internal enum Company {
+            /// The User is an Employee of this Company
+            case employee
 
-        /// The User is an Employee of this Company
-        case employee
+            /// The User is an external working Person
+            /// that is arranged in this Company
+            case externalWorker
 
-        /// The User is an external working Person
-        /// that is arranged in this Company
-        case externalWorker
+            /// A Customer of this Company
+            case customer
 
-        /// A Customer of this Company
-        case customer
+            /// A single Supplier of this Company.
+            case supplier
 
-        /// A single Supplier of this Company.
-        case supplier
+            /// The CEO of this Company
+            case ceo
 
-        /// The CEO of this Company
-        case ceo
+            /// The User is a Share Holder of this Company.
+            /// The Incomes and Expenses are dividents.
+            case shareholder
+        }
 
-        /// The User is a Share Holder of this Company.
-        /// The Incomes and Expenses are dividents.
-        case shareholder
-    }
+        /// The Relation if this Legal Person is an Organization
+        internal enum Organization {
+            /// The User is a member
+            /// of this Organization
+            case member
+        }
 
-    /// The Relation if this Legal Person is
-    /// a Person
-    internal enum PersonRelation : String, Relation {
-        var id : Self { self }
+        /// The Relation if this Legal Person is
+        /// a Person
+        internal enum Person {
+            /// The Legal Person that can accept Finances is
+            /// a Person of the Users Family
+            case family
 
-        /// The Legal Person that can accept Finances is
-        /// a Person of the Users Family
-        case family
+            /// The Legal Person is a friend of
+            /// the User
+            case friend
 
-        /// The Legal Person is a friend of
-        /// the User
-        case friend
-
-        /// The Person is a public figure
-        /// as a  Youtuber, Streamer or Influencer
-        case publicFigure
-    }
-
-    /// The Relation if this LEgal Person is an Organization
-    internal enum OrganizationRelation : String, Relation {
-        var id : Self { self }
-
-        /// The User is a member
-        /// of this Organization
-        case member
+            /// The Person is a public figure
+            /// as a  Youtuber, Streamer or Influencer
+            case publicFigure
+        }
     }
 
     internal var id : UUID = UUID()
@@ -88,7 +84,7 @@ internal class LegalPerson : Equatable, Identifiable, Codable {
 
     /// The Relation of this Legal Person and
     /// the User of this App
-    internal let relation : any Relation
+    internal let relation : Relation
 
     /// The Phone Number of this Legal Person
     internal let phone : String
@@ -103,6 +99,7 @@ internal class LegalPerson : Equatable, Identifiable, Codable {
         phone : String,
         notes : String
     ) {
+        self.relation = .Person.family
         self.name = name
         self.relation = relation
         self.phone = phone
