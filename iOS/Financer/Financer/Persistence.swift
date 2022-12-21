04d04ef771a6224aee48fd12c4384c8ccfbaa624
Julian Schumacher <Jules.media@outlook.de>
2022-12-21 08:23:52 +0100

    Implement models

--- iOS/Financer/Financer/Persistence.swift
+++ iOS/Financer/Financer/Persistence.swift
@@ -14,8 +14,8 @@ struct PersistenceController {
         let result = PersistenceController(inMemory: true)
         let viewContext = result.container.viewContext
         for _ in 0..<10 {
-            let newItem = Item(context: viewContext)
-            newItem.timestamp = Date()
+            let newItem = Finance(context: viewContext)
+            newItem.amount = 100
         }
         do {
             try viewContext.save()
