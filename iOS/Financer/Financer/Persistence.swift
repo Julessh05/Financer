688cbcefe27b9a96b8cec456c911fe7430f89fd2
Julian Schumacher <Jules.media@outlook.de>
2022-12-22 07:08:46 +0100

    Document Code

--- iOS/Financer/Financer/Persistence.swift
+++ iOS/Financer/Financer/Persistence.swift
@@ -25,15 +25,20 @@ internal struct PersistenceController {
     /// it contains a fatalError() Method which would crash the App
     /// if something went wrong while storing the Data.
     static var preview: PersistenceController = {
+        // Set location to /dev/null for preview.
         let result = PersistenceController(inMemory: true)
         let viewContext = result.container.viewContext
-        for _ in 0..<10 {
+        // Add some finances
+        for _ in 0 ..< 5 {
             let newItem = Finance(context: viewContext)
             newItem.amount = 100
         }
+        // try to save the Data.
         do {
             try viewContext.save()
         } catch {
+            // Crash App if Error appears in preview Controller.
+            // A Log should be generated alongside the console output.
             let nsError = error as NSError
             fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
         }
@@ -48,12 +53,22 @@ internal struct PersistenceController {
     let container: NSPersistentCloudKitContainer
 
     /// The Standard Init Method
-    /// to create a new persistence Controller
+    /// to create a new persistence Controller.
+    ///
+    /// The inMemory Parameter should only be set to true
+    /// for internal develop and test reasons, otherwise it will cause
+    /// uninteded behaviour.
     private init(inMemory: Bool = false) {
         container = NSPersistentCloudKitContainer(name: "Financer")
+        // If the inMemory Parameter is true,
+        // the container is stored to /dev/null which means is disappears and
+        // can't be found again.
+        //
+        // This should only be used in internal preview or test scenarios.
         if inMemory {
             container.persistentStoreDescriptions.first!.url = URL(fileURLWithPath: "/dev/null")
         }
+        // Load Data
         container.loadPersistentStores(completionHandler: { (storeDescription, error) in
             if let error = error as NSError? {
                 // Replace this implementation with code to handle the error appropriately.
@@ -70,6 +85,7 @@ internal struct PersistenceController {
                 fatalError("Unresolved error \(error), \(error.userInfo)")
             }
         })
+        // Automatically merge Changes.
         container.viewContext.automaticallyMergesChangesFromParent = true
     }
 }
