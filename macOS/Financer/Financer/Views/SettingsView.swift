88e4f8a1a6d3d6c3b8897944ceede69640933dc6
Julian Schumacher <Jules.media@outlook.de>
2023-02-17 11:42:11 +0100

    Work on Views

---
+++ macOS/Financer/Financer/Views/SettingsView.swift
@@ -0,0 +1,22 @@
+//
+//  SettingsView.swift
+//  Financer
+//
+//  Created by Julian Schumacher on 17.02.23.
+//
+
+import SwiftUI
+
+/// The struct representing the Settings of this App
+internal struct SettingsView: View {
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+internal struct SettingsView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingsView()
+    }
+}
