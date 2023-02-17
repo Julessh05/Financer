e75129313d81290519bd1759f0329be138700f59
Julian Schumacher <Jules.media@outlook.de>
2023-02-17 06:51:57 +0100

    Implement navigation bar

--- macOS/Financer/Financer/Views/Home.swift
+++ macOS/Financer/Financer/Views/Home.swift
@@ -12,18 +12,42 @@ import SwiftUI
 /// The Home View of this App, that is shown
 /// when opening the App.
 internal struct Home: View {
+    
+    /// The Finances fetched form the
+    /// Code Database.
+    @FetchRequest(
+        sortDescriptors: [
+            SortDescriptor(\Finance.date, order: .reverse)
+        ]
+    ) private var finances : FetchedResults<Finance>
+    
     var body: some View {
         NavigationSplitView {
             List {
                 HStack {
-                Image(systemName: "")
+                    Image(systemName: "house")
+                        .renderingMode(.original)
+                    Text("Home")
+                }
+                ForEach(finances) {
+                    finance in
+                    financesLabel(finance)
                 }
                 
             }
-        } content: {
-            
         } detail: {
             
+        }
+    }
+    
+    /// Builds, renders and returns the Label
+    /// for each Finance in the Navigation Bar
+    @ViewBuilder
+    private func financesLabel(_ finance : Finance) -> some View {
+        HStack {
+            Image(systemName: finance is Income ? "plus" : "minus")
+                .renderingMode(.original)
+            Text(String(finance.amount))
         }
     }
 }
@@ -31,5 +55,6 @@ internal struct Home: View {
 internal struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         Home()
+            .environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
     }
 }
