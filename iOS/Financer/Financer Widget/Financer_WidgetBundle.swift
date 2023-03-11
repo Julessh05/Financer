1d605394cc9f8359b06fd66f1cea87cd6014c283
Julian Schumacher <Jules.media@outlook.de>
2023-03-11 10:39:09 +0100

    Add Widget Extensions

---
+++ iOS/Financer/Financer Widget/Financer_WidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  Financer_WidgetBundle.swift
+//  Financer Widget
+//
+//  Created by Julian Schumacher on 10.03.23.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct Financer_WidgetBundle: WidgetBundle {
+    var body: some Widget {
+        Financer_Widget()
+    }
+}
