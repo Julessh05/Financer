a5a7d1e4235bdd315f58f19a2cd9949db65e5890
Julian Schumacher <Jules.media@outlook.de>
2023-04-24 16:32:53 +0200

    Add chart files

---
+++ iOS/Financer/Chart Overview/Chart_Overview.swift
@@ -0,0 +1,46 @@
+//
+//  Chart_Overview.swift
+//  Chart Overview
+//
+//  Created by Julian Schumacher on 24.04.23.
+//
+
+import WidgetKit
+import SwiftUI
+import Intents
+
+private struct Provider : IntentTimelineProvider {
+    
+}
+
+internal struct Chart_Overview: Widget {
+    let kind: String = "Chart_Overview"
+
+    var body: some WidgetConfiguration {
+        IntentConfiguration(
+            kind: kind,
+            intent: ConfigurationIntent.self,
+            provider: Provider()
+        ) {
+            entry in
+            ChartOverviewBody()
+        }
+        .configurationDisplayName("Chart Overview")
+        .description("Display an overview of your finances as a chart")
+    }
+}
+
+private struct ChartOverviewBody : View {
+    
+    var body: some View {
+        return Text("Test")
+    }
+    
+}
+
+internal struct Chart_Overview_Previews: PreviewProvider {
+    static var previews: some View {
+        ChartOverviewBody()
+            .previewContext(WidgetPreviewContext(family: .systemSmall))
+    }
+}
